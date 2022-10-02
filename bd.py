b4da3a27d9de01b564a560aceb0fa30661dfa5be
H0rn3t Sp1d3rs <97798085+H0rn3t-Sp1d3rs@users.noreply.github.com>
2022-10-03 00:10:12 +0600

    Create bd.py

---
+++ bd.py
@@ -0,0 +1,23 @@
+from googlesearch import search
+
+sex="""\033[1;32;36m
+	|~| Coded By : H0rn3t Sp1d3rs |~|
+	
+  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+  
+    [~] Github : https://github.com/h0rn3t-sp1d3rs
+    [~] Page     : https://www.facebook.com/Bads.community.bd
+    [~] Group    : https://www.facebook.com/groups/team.bads
+  
+  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+           
+"""
+print(sex)
+
+
+ok = "h0rn3t sp1d3rs"
+
+for i in search(ok, tld="co.in", num=5, stop=5, pause=2):
+    print(i)
+
